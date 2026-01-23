1dac59cd7cbde579fcf4706753438b67a8405ed6
libTSC <libtsc@icloud.com>
2026-01-24 00:30:58 +0800

    shitty mistake

--- src/net/telemetry.ts
+++ src/net/telemetry.ts
@@ -4,7 +4,7 @@ const hostname_regex = /^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])
 const ip_regex = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
 let proto = "http";
 export const Telemetry = {
-    enabled: boolean = false,
+    enabled: false,
     url: URL,
     async push(data: string, level: string) {
         if (!enabled) return;
